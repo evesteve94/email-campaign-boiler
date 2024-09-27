40e9989eb3dda92fd34dd65c34ec160b110c5281
Eva Björling <eva.bjorling@chasacademy.se>
2024-09-27 14:51:00 +0200

    mm

--- src/handleValidationErrors.ts
+++ src/handleValidationErrors.ts
@@ -1,12 +1,10 @@
 import { validationResult } from 'express-validator';
 import { Request, Response, NextFunction } from 'express-serve-static-core';
 
-const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
+export const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
   next();
 };
-
-export default handleValidationErrors;
